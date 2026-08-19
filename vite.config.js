b6c65690dc0e66a1b7549ad7b30761b73ad1d688
abhi hirve <abhipvt1906@gmail.com>
2026-08-19 13:22:44 +0530

    Update site

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
@@ -8,6 +9,12 @@ export default defineConfig({
     outDir: 'dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        jeecourse: resolve(__dirname, 'jeecourse/index.html'),
+        neetcourse: resolve(__dirname, 'neetcourse/index.html'),
+        foundationcourse: resolve(__dirname, 'foundationcourse/index.html')
+      },
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
