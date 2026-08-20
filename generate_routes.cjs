ea35e495aefb5aef8c5034bd623943df7ca6fa53
abhi hirve <abhipvt1906@gmail.com>
2026-08-20 10:37:04 +0530

    Update site

---
+++ generate_routes.cjs
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const routes = [
+  'courses',
+  'blogs',
+  'journey',
+  'about-us',
+  'reviews',
+  'contact-us',
+  'privacy',
+  'terms',
+  'sitemap',
+  'courses/iit-jee',
+  'courses/neet',
+  'courses/foundation'
+];
+
+const distDir = path.join(__dirname, 'dist');
+const indexHtmlPath = path.join(distDir, 'index.html');
+
+if (!fs.existsSync(indexHtmlPath)) {
+  console.error('dist/index.html not found!');
+  process.exit(1);
+}
+
+const indexHtml = fs.readFileSync(indexHtmlPath, 'utf8');
+
+routes.forEach(route => {
+  const routeDir = path.join(distDir, route);
+  if (!fs.existsSync(routeDir)) {
+    fs.mkdirSync(routeDir, { recursive: true });
+  }
+  fs.writeFileSync(path.join(routeDir, 'index.html'), indexHtml);
+});
+
+console.log('Successfully generated static index.html files for SPA routes.');
