be6acc54da622d90ae915bff175cbf5a7478c2f7
abhi hirve <abhipvt1906@gmail.com>
2026-09-02 15:30:28 +0530

    Updates

--- assets/contact-Df9ckCto.js
+++ assets/contact-Df9ckCto.js
@@ -1,4 +1,4 @@
-import{t as e}from"./main-UtERyNi5.js";function t(){return`
+import{t as e}from"./main-H8Gat0nG.js";function t(){return`
         <section class="contact-section" id="contact">
             <div class="section-header">
                 <h1>Get In Touch</h1>
