871326e719462a25a08e9b3dafc8c95cd2994452
abhi hirve <abhipvt1906@gmail.com>
2026-09-11 18:09:27 +0530

    Updates

--- assets/blogs-CyybONT9.js
+++ assets/blogs-CyybONT9.js
@@ -1,4 +1,4 @@
-import{ht as e,mt as t,pt as n,vt as r}from"./vendor-BpCc1Cic.js";import{r as i}from"./main-zORdpUBX.js";import{t as a}from"./courses-BKPhEzz0.js";var o=[`All Blogs`,`Study Hacks`,`JEE Mains PYQ`,`NEET Test Series`,`CBSE Class 10 PYQ`,`CBSE Class 12 PYQ`,`Foundation 9th-10th`],s=[];function c(){return`
+import{ht as e,mt as t,pt as n,vt as r}from"./vendor-BpCc1Cic.js";import{r as i}from"./main-BDS9ZIcH.js";import{t as a}from"./courses-BKPhEzz0.js";var o=[`All Blogs`,`Study Hacks`,`JEE Mains PYQ`,`NEET Test Series`,`CBSE Class 10 PYQ`,`CBSE Class 12 PYQ`,`Foundation 9th-10th`],s=[];function c(){return`
         <section class="blogs-section" id="blogs">
             <div class="er-pw-blog-container" id="blog-list-view">
                 
